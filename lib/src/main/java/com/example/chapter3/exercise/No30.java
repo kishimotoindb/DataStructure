df59c9154e438ce73bf685f78e44ca799579bc93
cuihaichen@macpro <519376470@qq.com>
2018-01-20 12:43:17 +0800

    chapter3 exercise:31

--- lib/src/main/java/com/example/chapter3/exercise/No30.java
+++ lib/src/main/java/com/example/chapter3/exercise/No30.java
@@ -1,8 +1,36 @@
 package com.example.chapter3.exercise;
 
+import com.example.BaseDataStructure.SingleNode;
+
 /**
  * Created by BigFaceBear on 2018.01.20
+ * 单链表实现栈结构
  */
 
 public class No30 {
+    SingleNode top, next;
+
+    void push(Integer elem) {
+        if (top != null) {
+            SingleNode newGay = new SingleNode(elem, top);
+            next = top;
+            top = newGay;
+        } else {
+            top = new SingleNode(elem, null);
+        }
+    }
+
+    Integer pop() {
+        if (top == null) return null;
+
+        Integer pop = top.item;
+
+        if (next != null) {
+            top = next;
+            next = next.next;
+        } else {
+            top = null;
+        }
+        return pop;
+    }
 }
